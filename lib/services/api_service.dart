ab51613381cc07a514a4d555e3b4375b16b44559
Vaishnavi Rai <2405398@kiit.ac.in>
2026-06-12 11:48:17 +0530

    Configure api service baseUrl to use local backend url

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 
 class ApiService {
-  static const String baseUrl = 'https://80205fdb31e59a.lhr.life/api';
+  static const String baseUrl = 'http://localhost:5000/api';
 
   static Future<String?> getToken() async {
     final prefs = await SharedPreferences.getInstance();
